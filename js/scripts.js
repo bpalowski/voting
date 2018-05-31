d802c3ce92191e04b752eb7718f7258f3a960614
Guest User <Guest@epicodus-70.local>
2018-05-31 14:15:44 -0700

    This now has HTML CSS JS B.S

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  var age = parseInt(prompt("How old are you?"));
+
+  if (age > 21) {
+    $('#over21').show();
+  }
+    else if (age < 21){
+    $('#under21').show();
+  }
+});
